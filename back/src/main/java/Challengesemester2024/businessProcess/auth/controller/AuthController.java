3844ad4e924da6291925eea6eb824e7573b9409a
minju <minju9711@naver.com>
2024-02-23 22:00:29 +0900

    modified : 검증부분과 실제 db 저장 부분을 나눔으로써 srp 원칙 수행 fix : 매니저와 보육원 둘 중 하나의 경우만 저장되는 버그 수정(@Transactional)

--- back/src/main/java/Challengesemester2024/businessProcess/auth/controller/AuthController.java
+++ back/src/main/java/Challengesemester2024/businessProcess/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import Challengesemester2024.businessProcess.auth.service.Email.EmailService;
 import Challengesemester2024.businessProcess.auth.service.PhoneNum.PhoneNumService;
 import Challengesemester2024.domain.childCenter.service.ChildCenterService;
 import Challengesemester2024.domain.manager.service.MangerService;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -32,6 +33,7 @@ public class AuthController {
     private final EmailService emailService;
     private final PhoneNumService phoneNumService;
 
+    @Transactional //매니저와 보육원의 동시저장을 보장해줄 애노테이션
     @PostMapping("/signUp")
     public ResponseEntity<?> authSignup(@RequestBody @Valid SignUpDto signUpDto, BindingResult bindingResult){
         handleBindingErrors(bindingResult);
@@ -42,13 +44,20 @@ public class AuthController {
         phoneNumService.checkVerifyNumberByPhoneNum(signUpDto.getManager().getPhoneNum(),
                 signUpDto.getManager().getPhoneVerificationCode());
 
-        //모든 인증번호가 맞을 시, 회원가입 진행
+        //각 db에 해당 값이 있는지 확인
+        mangerService.checkExits(signUpDto.getManager());
+        childCenterService.checkExits(signUpDto.getChildCenter());
+
+        //회원가입 진행
         mangerService.register(signUpDto.getManager());
         childCenterService.register(signUpDto.getChildCenter());
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    //"파사드 패턴" : 복잡한 서브시스템에 대한 간편한 인터페이스를 제공하는 디자인 패턴
+    // 이 패턴은 클라이언트와 복잡한 서브시스템 간의 의존성을 줄이고, 서브시스템을 더 쉽게 사용할 수 있도록 한다.
+    // 나중에 리팩토링 과정에서 더 자세하게 알아보면 좋을 것 같다.
 
     @PostMapping("/signIn")
     public ResponseEntity<?> authSignIn(@RequestBody @Valid SignInDto signDto, BindingResult bindingResult) {
