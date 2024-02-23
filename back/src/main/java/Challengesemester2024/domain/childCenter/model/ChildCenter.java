07c926f0a4c2ae03c2d92c5a602aeb7ee8d5aa37
minju <minju9711@naver.com>
2024-02-23 19:03:10 +0900

    modified : hibernate에 맞춰서 컬럼명 변경

--- back/src/main/java/Challengesemester2024/domain/childCenter/model/ChildCenter.java
+++ back/src/main/java/Challengesemester2024/domain/childCenter/model/ChildCenter.java
@@ -1,20 +1,20 @@
 package Challengesemester2024.domain.childCenter.model;
 
-import Challengesemester2024.domain.manager.model.Manager;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-@Table(name = "childCenter")
-public class ChildCenter {
 
+    @Entity
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+    @Table(name = "child_center")
+    public class ChildCenter {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long childCenter_id;
+    @Column(name = "child_center_id")
+    private Long id;
 
     @Column(nullable = false, unique = true, length = 255)
     private String phoneNumId;
@@ -36,7 +36,7 @@ public class ChildCenter {
 //    private String certificate;
 
     public ChildCenter(String ceoName, String centerName, String phoneNumId, String roadAddress, String detailAddress) {
-        this.childCenter_id=null;
+        this.id=null;
         this.ceoName = ceoName;
         this.centerName = centerName;
         this.phoneNumId = phoneNumId;
