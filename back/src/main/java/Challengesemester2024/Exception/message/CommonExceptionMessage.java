5a438b1de935c0a35ce536b2b473be51d9f84eac
minju <minju9711@naver.com>
2024-02-23 22:10:19 +0900

    modified : 이미 입력된 unique키와 동일한 정보 들어올 때의 에러처리과정 추가

--- back/src/main/java/Challengesemester2024/Exception/message/CommonExceptionMessage.java
+++ back/src/main/java/Challengesemester2024/Exception/message/CommonExceptionMessage.java
@@ -10,5 +10,7 @@ public class CommonExceptionMessage {
     public static final String  UsernameNotFoundException = "입력하신 이메일의 계정이 존재하지 않습니다.";
     public static final String ACCOUNT_PASSWORD_NOT_MATCH = "입력하신 Password가 일치하지 않습니다.";
     public static final String CenterByManagerNotFound = "해당 관리자와 연결된 보육원을 찾을 수 없습니다.";
+    public static final String DuplicatedUniqueKey = "이미 등록된 정보입니다. 다른 정보를 입력해주세요.";
+    public static final String UnVerifiedUserInfo = "인증을 먼저 진행해주세요.";
 
 }
