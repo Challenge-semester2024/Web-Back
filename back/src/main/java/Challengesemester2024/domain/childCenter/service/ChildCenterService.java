4ec56d7f0e826eab3ecc5cceb6a06f3872dd34ac
minju <minju9711@naver.com>
2024-02-26 16:27:19 +0900

    modify : s3의 사진 url 저장 위한 childService 수정

--- back/src/main/java/Challengesemester2024/domain/childCenter/service/ChildCenterService.java
+++ back/src/main/java/Challengesemester2024/domain/childCenter/service/ChildCenterService.java
@@ -3,6 +3,6 @@ package Challengesemester2024.domain.childCenter.service;
 import Challengesemester2024.businessProcess.auth.dto.SignUpDto;
 
 public interface ChildCenterService {
-    public void register(SignUpDto.ChildCenter childCenter);
-    public void checkExits(SignUpDto.ChildCenter childCenter);
+    public void register(SignUpDto.centerInfo childCenter, String fileUrl);
+    public void checkExits(SignUpDto.centerInfo childCenter);
 }
