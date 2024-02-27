bf0ffdec7f67802b2a8f3def6435688f962454bb
minju <minju9711@naver.com>
2024-02-27 21:46:44 +0900

    add : db 초기 생성 및 관계 설정해줄 파사드 클래스 생성

--- back/src/main/java/Challengesemester2024/businessProcess/Facade/DatabaseFacadeServiceImpl.java
+++ back/src/main/java/Challengesemester2024/businessProcess/Facade/DatabaseFacadeServiceImpl.java
@@ -1,4 +1,26 @@
 package Challengesemester2024.businessProcess.Facade;
 
+import Challengesemester2024.businessProcess.auth.dto.auth.S3urlDto;
+import Challengesemester2024.businessProcess.auth.dto.auth.SignUpDto;
+import Challengesemester2024.domain.childCenter.service.ChildCenterService;
+import Challengesemester2024.domain.manager.service.ManagerService;
+import Challengesemester2024.domain.routeInfo.domain.RouteInfo;
+import Challengesemester2024.domain.routeInfo.service.RouteInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class DatabaseFacadeServiceImpl implements DatabaseFacadeService{
+    private final RouteInfoService routeInfoService;
+    private final ManagerService managerService;
+    private final ChildCenterService childCenterService;
+    @Override
+    public void createDbAndRelationsWhenSignUp(SignUpDto signUpDto, S3urlDto s3urlDto) {
+        //1. 찾아오는 길 db 생성
+        RouteInfo routeInfo = routeInfoService.createRouteInfo();
+
+
+
+    }
 }
