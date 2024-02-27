97565b55c391a98b28730981d937456c918eda75
minju <minju9711@naver.com>
2024-02-27 21:37:12 +0900

    add : db 첫 생성 시 넣을 초기 메시지 상수클래스 정의

---
+++ back/src/main/java/Challengesemester2024/config/DbInitConstants.java
@@ -0,0 +1,7 @@
+package Challengesemester2024.config;
+
+public class DbInitConstants {
+    public static final String routeInfoInitMessage = "찾아오는 길을 입력해주세요!";
+    public static final String greetingsInitMessage = "환영인사를 적어주세요!";
+    public static final String facilityInitMessage = "시설소개를 작성해주세요!";
+}
