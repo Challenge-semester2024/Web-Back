d2628009040da53712dff68456c6d3b2461c086a
minju <minju9711@naver.com>
2024-02-28 01:09:31 +0900

    modify : IoException 예외처리 경우 추가

--- back/src/main/java/Challengesemester2024/Exception/collections/IoException/NotExitsInitImageFile.java
+++ back/src/main/java/Challengesemester2024/Exception/collections/IoException/NotExitsInitImageFile.java
@@ -9,3 +9,4 @@ public class NotExitsInitImageFile extends IOException {
         super(DbInitExceptionMessage.NoExitsInitImageFile);
     }
 }
+//참고 : IOexception은 체크예외에 해당되어서, 무조건 함수선언부 옆에 throws를 적어줘야함. runtimeExcepton은 비체크예외라 안적어도 됨
