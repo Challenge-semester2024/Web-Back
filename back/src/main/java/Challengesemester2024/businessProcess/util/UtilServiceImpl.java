759c0fa03515bb3fa2b4d824204a54d58d79a6c9
minju <minju9711@naver.com>
2024-02-28 13:46:03 +0900

    add : 초기이미지 파일 url 가져오는 기능 생성

--- back/src/main/java/Challengesemester2024/businessProcess/util/UtilServiceImpl.java
+++ back/src/main/java/Challengesemester2024/businessProcess/util/UtilServiceImpl.java
@@ -1,12 +1,20 @@
 package Challengesemester2024.businessProcess.util;
 
+import Challengesemester2024.Exception.collections.IoException.NotExitsInitImageFile;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
+import software.amazon.ion.IonException;
 
+import java.io.IOException;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Component
+@RequiredArgsConstructor
 public class UtilServiceImpl implements UtilService {
+    private ResourceLoader resourceLoader;
     @Override
     public String getRandomNum() {
         return String.valueOf(ThreadLocalRandom.current().nextInt(100000,900000));
@@ -16,4 +24,11 @@ public class UtilServiceImpl implements UtilService {
         String uuid = UUID.randomUUID().toString();
         return uuid+originalName;
     }
+
+    @Override
+    public String getInitImagePath(String fileName) throws IOException { //해당 에러 발생시, 상위 함수로 에러 던져짐
+        Resource resource = resourceLoader.getResource("classpath:static/image/" + fileName);
+        return resource.getFile().getAbsolutePath();
+    }
+
 }
