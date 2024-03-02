883714dbd11a5ae2b655dbcc6a94ff5e52e15a7c
minju <minju9711@naver.com>
2024-03-03 02:54:49 +0900

    modify : 이미지 파일 입출력 과정에서 오류 발생할 시 사용할 상수변수 생성

--- back/src/main/java/Challengesemester2024/Exception/message/DbInitExceptionMessage.java
+++ back/src/main/java/Challengesemester2024/Exception/message/DbInitExceptionMessage.java
@@ -2,4 +2,5 @@ package Challengesemester2024.Exception.message;
 
 public class DbInitExceptionMessage {
     public static final String NoExitsInitImageFile = "기본 프로필 사진의 위치를 찾을 수 없습니다.";
+    public static final String AllImageFileIOError = "파일 입출력 과정에서 오류가 발생했습니다.";
 }
