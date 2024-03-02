1855d5b43a8f3a9daf8949fbce976b3b91262028
minju <minju9711@naver.com>
2024-03-03 02:48:12 +0900

    rename : 클래스 위치 수정

--- back/src/main/java/Challengesemester2024/config/DbInitConstants.java
+++
@@ -1,8 +0,0 @@
-package Challengesemester2024.config;
-
-public class DbInitConstants {
-    public static final String routeInfoInitMessage = "찾아오는 길을 입력해주세요!";
-    public static final String greetingsInitMessage = "환영인사를 적어주세요!";
-    public static final String initImageFileName = "InitProfile.jpeg";
-    public static final int facilityInitTotalArea = 0;
-}
