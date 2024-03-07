42f4bf933ceaac20ffc252f2564fbc51c8fc98f2
minju <minju9711@naver.com>
2024-03-07 15:15:23 +0900

    modify : 양방향 설계를 위해 시설소개 db 수정

--- back/src/main/java/Challengesemester2024/domain/facilityIntroduction/model/FacilityIntroduction.java
+++ back/src/main/java/Challengesemester2024/domain/facilityIntroduction/model/FacilityIntroduction.java
@@ -3,6 +3,8 @@ package Challengesemester2024.domain.facilityIntroduction.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Getter
 @AllArgsConstructor(access =  AccessLevel.PROTECTED)
@@ -17,4 +19,13 @@ public class FacilityIntroduction {
     @Column(nullable = false, length = 255)
     private int totalArea; //각 층 시설 합계면적
 
+    //양방향 설계를 위한 필드들
+    @OneToMany(mappedBy = "facility_picture", fetch = FetchType.LAZY)
+    private List<FacilityPicture> facilityPictures;
+
+    @OneToMany(mappedBy = "facility_size", fetch = FetchType.LAZY)
+    private List<FacilitySize> facilitySizes;
+
+    @OneToMany(mappedBy = "floor_picture_cluster", fetch = FetchType.LAZY)
+    private List<FloorPictureCluster> FloorPictureClusters;
 }
