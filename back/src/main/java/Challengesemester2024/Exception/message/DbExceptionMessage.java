b77d9072ea99c2d3bc19845f35cd95b23d755f87
minju <minju9711@naver.com>
2024-03-09 22:13:58 +0900

    modify : join 시 해당 db를 찾지 못했을때 반환할 에러메시지 상수변수 생성

--- back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
+++ back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
@@ -1,6 +1,7 @@
 package Challengesemester2024.Exception.message;
 
-public class DbInitExceptionMessage {
+public class DbExceptionMessage {
     public static final String NoExitsInitImageFile = "기본 프로필 사진의 위치를 찾을 수 없습니다.";
     public static final String AllImageFileIOError = "파일 입출력 과정에서 오류가 발생했습니다.";
+    public static final String DatabaseNotFoundException = "해당 DB를 찾을 수 없습니다.";
 }
