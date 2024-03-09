ec7b004b5aa5feacd60eaf9f0a4a76160f31a631
minju <minju9711@naver.com>
2024-03-09 22:15:03 +0900

    add : 시설소개의 외래키 가져올 queryDsl 이용한 레파지토리 클래스 생성

---
+++ back/src/main/java/Challengesemester2024/domain/facility/facilityIntroduction/repository/FacilityIntroRepositoryCustom.java
@@ -0,0 +1,8 @@
+package Challengesemester2024.domain.facility.facilityIntroduction.repository;
+
+import Challengesemester2024.domain.facility.facilityIntroduction.dto.GetFacilityIntroPKDto;
+
+public interface FacilityIntroRepositoryCustom {
+    // 이메일로 관리자db->보육원db로 외래키를 타고 들어가 보육원 필드에 저장된 시설소개의 기본키 가져오기
+    public GetFacilityIntroPKDto getFacilityPk(String email);
+}
