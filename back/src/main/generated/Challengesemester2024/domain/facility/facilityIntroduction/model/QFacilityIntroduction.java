6c2711e293f74b1abcad5713fc95c3fff5b28660
minju <minju9711@naver.com>
2024-03-09 22:21:37 +0900

    add : 각 entity에 할당되어 만들어진 Q클래스들

---
+++ back/src/main/generated/Challengesemester2024/domain/facility/facilityIntroduction/model/QFacilityIntroduction.java
@@ -0,0 +1,44 @@
+package Challengesemester2024.domain.facility.facilityIntroduction.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QFacilityIntroduction is a Querydsl query type for FacilityIntroduction
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFacilityIntroduction extends EntityPathBase<FacilityIntroduction> {
+
+    private static final long serialVersionUID = -1320390913L;
+
+    public static final QFacilityIntroduction facilityIntroduction = new QFacilityIntroduction("facilityIntroduction");
+
+    public final ListPath<Challengesemester2024.domain.facility.floorPictureCluster.model.FloorPictureCluster, Challengesemester2024.domain.facility.floorPictureCluster.model.QFloorPictureCluster> floorPictureClusters = this.<Challengesemester2024.domain.facility.floorPictureCluster.model.FloorPictureCluster, Challengesemester2024.domain.facility.floorPictureCluster.model.QFloorPictureCluster>createList("floorPictureClusters", Challengesemester2024.domain.facility.floorPictureCluster.model.FloorPictureCluster.class, Challengesemester2024.domain.facility.floorPictureCluster.model.QFloorPictureCluster.class, PathInits.DIRECT2);
+
+    public final ListPath<Challengesemester2024.domain.facility.floorSize.model.FloorSize, Challengesemester2024.domain.facility.floorSize.model.QFloorSize> floorSizes = this.<Challengesemester2024.domain.facility.floorSize.model.FloorSize, Challengesemester2024.domain.facility.floorSize.model.QFloorSize>createList("floorSizes", Challengesemester2024.domain.facility.floorSize.model.FloorSize.class, Challengesemester2024.domain.facility.floorSize.model.QFloorSize.class, PathInits.DIRECT2);
+
+    public final StringPath id = createString("id");
+
+    public final NumberPath<Integer> totalArea = createNumber("totalArea", Integer.class);
+
+    public QFacilityIntroduction(String variable) {
+        super(FacilityIntroduction.class, forVariable(variable));
+    }
+
+    public QFacilityIntroduction(Path<? extends FacilityIntroduction> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFacilityIntroduction(PathMetadata metadata) {
+        super(FacilityIntroduction.class, metadata);
+    }
+
+}
+
---
+++ back/src/main/generated/Challengesemester2024/domain/facility/floorPictureCluster/model/QFloorPictureCluster.java
@@ -0,0 +1,55 @@
+package Challengesemester2024.domain.facility.floorPictureCluster.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QFloorPictureCluster is a Querydsl query type for FloorPictureCluster
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFloorPictureCluster extends EntityPathBase<FloorPictureCluster> {
+
+    private static final long serialVersionUID = 133127017L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QFloorPictureCluster floorPictureCluster = new QFloorPictureCluster("floorPictureCluster");
+
+    public final Challengesemester2024.domain.facility.facilityIntroduction.model.QFacilityIntroduction facilityIntroduction;
+
+    public final ListPath<Challengesemester2024.domain.facility.floorPicutre.model.FloorPicture, Challengesemester2024.domain.facility.floorPicutre.model.QFloorPicture> floorPicture = this.<Challengesemester2024.domain.facility.floorPicutre.model.FloorPicture, Challengesemester2024.domain.facility.floorPicutre.model.QFloorPicture>createList("floorPicture", Challengesemester2024.domain.facility.floorPicutre.model.FloorPicture.class, Challengesemester2024.domain.facility.floorPicutre.model.QFloorPicture.class, PathInits.DIRECT2);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath picuture = createString("picuture");
+
+    public QFloorPictureCluster(String variable) {
+        this(FloorPictureCluster.class, forVariable(variable), INITS);
+    }
+
+    public QFloorPictureCluster(Path<? extends FloorPictureCluster> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QFloorPictureCluster(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QFloorPictureCluster(PathMetadata metadata, PathInits inits) {
+        this(FloorPictureCluster.class, metadata, inits);
+    }
+
+    public QFloorPictureCluster(Class<? extends FloorPictureCluster> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.facilityIntroduction = inits.isInitialized("facilityIntroduction") ? new Challengesemester2024.domain.facility.facilityIntroduction.model.QFacilityIntroduction(forProperty("facilityIntroduction")) : null;
+    }
+
+}
+
