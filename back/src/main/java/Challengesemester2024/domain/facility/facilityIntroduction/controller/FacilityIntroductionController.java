5f1fc6411f4e60f461e7d23f1845a11a3a68e353
minju <minju9711@naver.com>
2024-03-10 15:22:12 +0900

    modify : 정확한 책임전가를 위해 시설소개 컨트롤러의 내용을 층별규모 컨트롤러로 이동

--- back/src/main/java/Challengesemester2024/domain/facility/facilityIntroduction/controller/FacilityIntroductionController.java
+++ back/src/main/java/Challengesemester2024/domain/facility/facilityIntroduction/controller/FacilityIntroductionController.java
@@ -1,17 +1,7 @@
 package Challengesemester2024.domain.facility.facilityIntroduction.controller;
 
-import Challengesemester2024.Exception.collections.InputValid.BindingErrors;
-import Challengesemester2024.domain.facility.floorSize.dto.UpdateFloorSizeDto;
-import Challengesemester2024.domain.facility.facilityIntroduction.service.FacilityService;
-import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,21 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class FacilityIntroductionController {
-    private final FacilityService facilityService;
 
-    @Transactional
-    @PostMapping("/update/floorSize")
-    public ResponseEntity<?> updateFloorSize(@RequestBody @Valid UpdateFloorSizeDto updateFloorSizeDto, BindingResult bindingResult){
-        handleBindingErrors(bindingResult);
-        facilityService.UpdateFloorSize(updateFloorSizeDto);
-
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    public void handleBindingErrors(BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            throw new BindingErrors();
-        }
-    }
 
 }
