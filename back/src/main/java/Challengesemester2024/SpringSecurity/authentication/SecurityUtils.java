bc0e97ef72e3402cc96fb79f911693462ff66d63
minju <minju9711@naver.com>
2024-03-10 15:19:43 +0900

    주석추가

--- back/src/main/java/Challengesemester2024/SpringSecurity/authentication/SecurityUtils.java
+++ back/src/main/java/Challengesemester2024/SpringSecurity/authentication/SecurityUtils.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecurityUtils {
 
-    public AuthenticatedEmailDTO getAuthenticationEmail(){
+    public AuthenticatedEmailDTO getAuthenticationEmail(){ // SecurityContextHolder에서 인증 정보를 얻어 현재 사용자의 이메일 확인하기
         Authentication authentication = (Authentication) SecurityContextHolder.getContext().getAuthentication();
 
         if(!authentication.isAuthenticated()){ //인증된 상태가 아닐 때, 에러 처리
