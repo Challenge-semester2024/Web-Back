a43742c663b9f87a42f8d239a881b167876f608a
minju <minju9711@naver.com>
2024-03-10 15:13:29 +0900

    modify : 업데이트 된 층별규모 db의 내용을 시설소개 db에도 반영(양방향 매핑 위함) refactor : 인증이메일 가져오는 로직 srp 위해 분리

--- back/src/main/java/Challengesemester2024/domain/facility/floorSize/service/FloorServiceImpl.java
+++ back/src/main/java/Challengesemester2024/domain/facility/floorSize/service/FloorServiceImpl.java
@@ -1,7 +1,7 @@
 package Challengesemester2024.domain.facility.floorSize.service;
 
 import Challengesemester2024.SpringSecurity.authentication.AuthenticatedEmailDTO;
-import Challengesemester2024.SpringSecurity.authentication.SecurityUtils;
+import Challengesemester2024.domain.facility.dto.FacilityFloorSizeUpdateRequest;
 import Challengesemester2024.domain.facility.facilityIntroduction.dto.GetFacilityIntroPKDto;
 import Challengesemester2024.domain.facility.facilityIntroduction.repository.FacilityIntroRepository;
 import Challengesemester2024.domain.facility.floorSize.dto.UpdateFloorSizeDto;
@@ -13,18 +13,14 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class FloorServiceImpl implements FloorService{
-    private final SecurityUtils securityUtils;
     private final FacilityIntroRepository facilityIntroRepository;
     private final FloorRepository floorRepository;
     @Override
-    public void UpdateFloorSize(UpdateFloorSizeDto updateFloorSizeDto) {
-        // 1. SecurityContextHolder에서 인증 정보를 얻어 현재 사용자의 이메일 확인하기
-        AuthenticatedEmailDTO authenticatedEmailDTO = securityUtils.getAuthenticationEmail();
+    public FacilityFloorSizeUpdateRequest UpdateFloorSize(UpdateFloorSizeDto updateFloorSizeDto, AuthenticatedEmailDTO authenticatedEmailDto) {
+        //1. 해당 이메일로 관리자db->보육원db로 외래키를 타고 들어가 보육원 필드에 저장된 시설소개의 기본키 가져오기
+        GetFacilityIntroPKDto getFacilityIntroPKDto = facilityIntroRepository.getFacilityPk(authenticatedEmailDto.getEmail());
 
-        //2. 해당 이메일로 관리자db->보육원db로 외래키를 타고 들어가 보육원 필드에 저장된 시설소개의 기본키 가져오기
-        GetFacilityIntroPKDto getFacilityIntroPKDto = facilityIntroRepository.getFacilityPk(authenticatedEmailDTO.getEmail());
-
-        //3. 가져온 기본키와 외부에서 받은 updateFloorSizeDto로 floorPicutre db 저장하기
+        //2. 가져온 기본키와 외부에서 받은 updateFloorSizeDto로 floorPicutre db 저장하기
         FloorSize floorSize = FloorSize.builder()
                 .floor(updateFloorSizeDto.getFloor())
                 .area(updateFloorSizeDto.getArea())
@@ -33,6 +29,15 @@ public class FloorServiceImpl implements FloorService{
                 .facilityIntroduction(getFacilityIntroPKDto.getFacilityIntroduction())
                 .build();
 
+        //3. 층별db에 내용 저장
         floorRepository.save(floorSize);
+
+        //4. 양방향 매핑 위해 dto 생성
+        FacilityFloorSizeUpdateRequest facilityFloorSizeUpdateRequest = FacilityFloorSizeUpdateRequest.builder()
+                .facilityIntroPKDto(getFacilityIntroPKDto)
+                .floorSize(floorSize)
+                .build();
+
+        return facilityFloorSizeUpdateRequest;
     }
 }
