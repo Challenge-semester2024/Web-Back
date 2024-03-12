ad81caaac7b0bed8331331c12d70eb2592c1c961
minju <minju9711@naver.com>
2024-03-12 18:33:12 +0900

    feat : 시설파사드 인터페이스에 층별사진업데이트 함수 추가

--- back/src/main/java/Challengesemester2024/domain/facility/facade/FacilityFacadeService.java
+++ back/src/main/java/Challengesemester2024/domain/facility/facade/FacilityFacadeService.java
@@ -1,7 +1,9 @@
 package Challengesemester2024.domain.facility.facade;
 
+import Challengesemester2024.domain.facility.floorPicutre.dto.UpdateFloorPictureDto;
 import Challengesemester2024.domain.facility.floorSize.dto.UpdateFloorSizeDto;
 
 public interface FacilityFacadeService {
     void updateFloorSize(UpdateFloorSizeDto updateFloorSizeDto);
+    void updateFloorPicture(UpdateFloorPictureDto updateFloorPictureDto);
 }
