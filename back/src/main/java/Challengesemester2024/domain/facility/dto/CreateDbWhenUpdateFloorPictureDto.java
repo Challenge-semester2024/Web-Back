178abfe4cdc47cf2373ff94c6cdc1622fcbb9c1f
minju <minju9711@naver.com>
2024-04-02 22:12:31 +0900

    add : floorPicture 업데이트를 위한 dto 수정

--- back/src/main/java/Challengesemester2024/domain/facility/dto/CreateDbWhenUpdateFloorPictureDto.java
+++ back/src/main/java/Challengesemester2024/domain/facility/dto/CreateDbWhenUpdateFloorPictureDto.java
@@ -1,18 +1,17 @@
 package Challengesemester2024.domain.facility.dto;
 
 import Challengesemester2024.domain.facility.facilityIntroduction.model.FacilityIntroduction;
-import Challengesemester2024.domain.facility.floorPictureCluster.model.FloorPictureCluster;
 import Challengesemester2024.domain.facility.floorPicutre.dto.FloorPictureDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.security.core.Authentication;
 
 @Data
 @AllArgsConstructor
 @Builder
 public class CreateDbWhenUpdateFloorPictureDto {
     FloorPictureDto floorPictureDto;
-    FloorPictureCluster floorPictureCluster;
-    String email;
     FacilityIntroduction facilityIntroduction;
+    Authentication authentication;
 }
