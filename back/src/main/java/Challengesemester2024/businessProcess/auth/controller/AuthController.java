9707eb9bef16d01e036db9f234ce497f60c50ed3
minju <minju9711@naver.com>
2024-04-02 19:18:39 +0900

    add : controller에 swagger 적용

--- back/src/main/java/Challengesemester2024/businessProcess/auth/controller/AuthController.java
+++ back/src/main/java/Challengesemester2024/businessProcess/auth/controller/AuthController.java
@@ -4,8 +4,14 @@ import Challengesemester2024.Exception.collections.InputValid.BindingErrors;
 import Challengesemester2024.SpringSecurity.jwt.dto.AllJwtTokenDto;
 import Challengesemester2024.businessProcess.auth.dto.auth.SignInDto;
 import Challengesemester2024.businessProcess.auth.dto.auth.SignUpDto;
-import Challengesemester2024.businessProcess.facade.service.AuthFacadeService;
+import Challengesemester2024.businessProcess.auth.service.Facade.AuthFacadeService;
 import Challengesemester2024.config.constant.ControllerConstants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+@Tag(name = "Auth", description = "auth Api")
 @RestController
 @RequestMapping("api/auth")
 @RequiredArgsConstructor
@@ -27,6 +34,12 @@ public class AuthController {
 
     @Transactional //매니저와 보육원의 동시저장을 보장해줄 애노테이션
     @PostMapping("/signUp")
+    @Operation(summary = "signUp Api summary", description = "회원가입 시 사용할 api 명세서")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "success",
+            content = {@Content(schema = @Schema(implementation = AuthController.class))}),
+            @ApiResponse(responseCode = "400", description = "Not founc"),
+    })
     public ResponseEntity<?> authSignup(@RequestPart("signUpDto") @Valid SignUpDto signUpDto,
                                         @RequestPart("certificateFile") MultipartFile multipartFile,
                                         BindingResult bindingResult ) throws IOException {
