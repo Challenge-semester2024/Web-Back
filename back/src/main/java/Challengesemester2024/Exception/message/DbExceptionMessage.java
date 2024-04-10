d5748cce8c224f01db1b05865cdbc0f0ad178b7f
minju <minju9711@naver.com>
2024-04-10 19:52:19 +0900

    add : db 접근 시 발생할 수 있는 예외처리

--- back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
+++ back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
@@ -3,5 +3,8 @@ package Challengesemester2024.Exception.message;
 public class DbExceptionMessage {
     public static final String NoExitsInitImageFile = "기본 프로필 사진의 위치를 찾을 수 없습니다.";
     public static final String AllImageFileIOError = "파일 입출력 과정에서 오류가 발생했습니다.";
-    public static final String DatabaseNotFoundException = "해당 DB를 찾을 수 없습니다.";
+    public static final String FacilityDatabaseNotFoundException = "해당하는 시설소개 DB를 찾을 수 없습니다.";
+    public static final String FloorPictureDatabaseNotFoundException = "해당하는 층별사진 DB를 찾을 수 없습니다.";
+    public static final String FloorSizeDatabaseNotFoundException = "해당하는 층별규모 DB를 찾을 수 없습니다.";
+    public static final String ChildCenterDatabaseNotFoundException = "해당하는 보육원 DB를 찾을 수 없습니다.";
 }
