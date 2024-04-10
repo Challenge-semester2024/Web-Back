81b9e23bb4786ee811153269a6af899085bb65c3
minju <minju9711@naver.com>
2024-04-10 19:49:27 +0900

    add : 로직 성공 시 클라이언트에게 반환할 메시지 상수변수 생성

--- back/src/main/java/Challengesemester2024/config/constant/ControllerConstants.java
+++ back/src/main/java/Challengesemester2024/config/constant/ControllerConstants.java
@@ -3,4 +3,7 @@ package Challengesemester2024.config.constant;
 public class ControllerConstants {
     public static final String completeSignUp = "회원가입이 완료되었습니다.";
     public static final String completeUpdateFloorSize = "층별 소개가 업데이트 완료되었습니다.";
+    public static final String completeUpdateYearHistory = "연혁이 업데이트 완료되었습니다.";
+    public static final String completeUpdateGreetings = "인삿말이 업데이트 완료되었습니다.";
+    public static final String completeUpdateRouteInfo = "찾아오는길이 업데이트 완료되었습니다.";
 }
