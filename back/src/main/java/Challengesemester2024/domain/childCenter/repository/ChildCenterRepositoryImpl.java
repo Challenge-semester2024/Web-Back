965f3c7e350adb0218e6bc713a63b2cc319de423
minju <minju9711@naver.com>
2024-04-10 19:46:06 +0900

    add : childCenter pk 값 반환하는 로직 추가

--- back/src/main/java/Challengesemester2024/domain/childCenter/repository/ChildCenterRepositoryImpl.java
+++ back/src/main/java/Challengesemester2024/domain/childCenter/repository/ChildCenterRepositoryImpl.java
@@ -1,8 +1,35 @@
 package Challengesemester2024.domain.childCenter.repository;
 
+import Challengesemester2024.Exception.collections.business.DatabaseNotFoundException;
+import Challengesemester2024.Exception.message.DbExceptionMessage;
+import Challengesemester2024.domain.childCenter.model.ChildCenter;
+import Challengesemester2024.domain.childCenter.model.QChildCenter;
+import Challengesemester2024.domain.manager.model.QManager;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+
+@RequiredArgsConstructor
 public class ChildCenterRepositoryImpl implements ChildCenterRepositoryCustom {
+    private final JPAQueryFactory jpaQueryFactory;
+
     @Override
-    public Long getCenterIdByManagerAccountId(String accountId) {
-        return null;
+    public ChildCenter getChildCenterPk(Authentication authentication) {
+        QManager qManager = QManager.manager;
+        QChildCenter qChildCenter = QChildCenter.childCenter;
+
+        ChildCenter fetchedChildCenter = jpaQueryFactory
+                .select(qChildCenter)
+                .from(qManager)
+                .join(qManager.childCenter, qChildCenter)
+                .where(qManager.emailId.eq(authentication.getName()))
+                .fetchOne();
+
+        if (fetchedChildCenter == null) {
+            throw new DatabaseNotFoundException(DbExceptionMessage.ChildCenterDatabaseNotFoundException);
+        }
+
+        return fetchedChildCenter;
+
     }
 }
