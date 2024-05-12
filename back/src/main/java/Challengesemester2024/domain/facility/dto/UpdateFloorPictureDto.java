b88068b07aa30b42cf190f448eeb69a88e649d88
minju <minju9711@naver.com>
2024-05-13 04:53:00 +0900

    remove : 로직 대거 수정으로 인해 쓰지않는 dto 삭제

--- back/src/main/java/Challengesemester2024/domain/facility/dto/UpdateFloorPictureDto.java
+++ back/src/main/java/Challengesemester2024/domain/facility/dto/UpdateFloorPictureDto.java
@@ -1,14 +1,14 @@
-package Challengesemester2024.domain.facility.dto;
-
-import Challengesemester2024.domain.facility.floorPicutre.model.FloorPicture;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-@Builder
-public class UpdateFloorPictureDto {
-    FloorPicture oldFloorPicture;
-    FloorPicture newFloorPicture;
-}
+//package Challengesemester2024.domain.facility.dto;
+//
+//import Challengesemester2024.domain.facility.floorPicutre.model.FloorPicture;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Data;
+//
+//@Data
+//@AllArgsConstructor
+//@Builder
+//public class UpdateFloorPictureDto {
+//    FloorPicture oldFloorPicture;
+//    FloorPicture newFloorPicture;
+//}
