126cb055a3837c8168dcc1c030dfaaa775e196f8
minju <minju9711@naver.com>
2024-06-01 19:45:28 +0900

    [modify] jwt 레디스 서비스 클래스 수정

---
+++ back/src/main/java/Challengesemester2024/businessProcess/redis/service/JwtRedisService.java
@@ -0,0 +1,11 @@
+package Challengesemester2024.businessProcess.redis.service;
+
+import Challengesemester2024.SpringSecurity.jwt.redis.model.RedisRefreshTokenDto;
+
+import java.util.Optional;
+
+public interface JwtRedisService {
+    RedisRefreshTokenDto save(RedisRefreshTokenDto redisRefreshTokenDto);
+    //리프레시 토큰을 redis 에 저장
+    Optional<RedisRefreshTokenDto> find(String id);
+}
