4ae9ff243b0d781b2d92052132bc9a367d4cdd25
minju <minju9711@naver.com>
2024-06-01 18:48:30 +0900

    [add] : 안쓰는 폴더 삭제

--- back/src/main/java/Challengesemester2024/Exception/collections/business/ManagerAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package Challengesemester2024.Exception.collections.business;
-
-import Challengesemester2024.Exception.message.CommonExceptionMessage;
-
-public class ManagerAlreadyExistsException extends BusinessException {
-    public ManagerAlreadyExistsException() {
-        super(CommonExceptionMessage.ManagerAlreadyExists);
-    }
-}
