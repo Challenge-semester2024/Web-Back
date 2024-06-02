8d96f59335bfd99caf61bbd82b0e574073093d1e
minju <minju9711@naver.com>
2024-06-02 23:29:43 +0900

    feat : jwt 어세스 토큰 재발급 로직 수정

--- back/src/main/java/Challengesemester2024/SpringSecurity/jwt/controller/JwtController.java
+++ back/src/main/java/Challengesemester2024/SpringSecurity/jwt/controller/JwtController.java
@@ -2,22 +2,35 @@ package Challengesemester2024.SpringSecurity.jwt.controller;
 
 import Challengesemester2024.SpringSecurity.jwt.dto.AccessTokenDto;
 import Challengesemester2024.SpringSecurity.jwt.service.JwtService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class JwtController {
     private final JwtService jwtService;
 
-    @PostMapping("/update/AccessToken")
+    @Transactional
+    @Operation(summary = "request new access Token", description = "어세스 토큰 재발급")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved child centers",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AccessTokenDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid input",
+                    content = @Content)
+    })
+    @PostMapping("/auth/update/access/token")
     public AccessTokenDto updateAccessToken(HttpServletRequest request){
         return jwtService.updateAccessToken(request);
     }
-
 
 }
