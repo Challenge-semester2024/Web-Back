0e7630a30a812dfd8ac51ee0e06353c8a4f707a3
minju <minju9711@naver.com>
2024-06-02 23:06:47 +0900

    feat : 인증 객체로 봉사자 db 찾아오는 함수 추가 feat : id로 봉사자 db 찾아오는 함수 추가

---
+++ back/src/main/java/Challengesemester2024/domain/volunteer/dto/VolunteerResponseDto.java
@@ -0,0 +1,11 @@
+package Challengesemester2024.domain.volunteer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class VolunteerResponseDto {
+    private Long id;
+    private String name;
+}
