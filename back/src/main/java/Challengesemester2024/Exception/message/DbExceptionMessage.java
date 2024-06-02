539a19d53cc33f32a8f7ef9ac9686db92c7e0654
minju <minju9711@naver.com>
2024-06-02 23:00:41 +0900

    feat : 앱 에서의 에러 처리 static 변수 추가

--- back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
+++ back/src/main/java/Challengesemester2024/Exception/message/DbExceptionMessage.java
@@ -7,4 +7,7 @@ public class DbExceptionMessage {
     public static final String FloorPictureDatabaseNotFoundException = "해당하는 층별사진 DB를 찾을 수 없습니다.";
     public static final String FloorSizeDatabaseNotFoundException = "해당하는 층별규모 DB를 찾을 수 없습니다.";
     public static final String ChildCenterDatabaseNotFoundException = "해당하는 보육원 DB를 찾을 수 없습니다.";
+    public static final String VolunteerDatabaseNotFoundException = "해당하는 봉사자 DB를 찾을 수 없습니다.";
+    public static final String RecruitmentDatabaseNotFoundException = "해당하는 봉사 공고 DB를 찾을 수 없습니다.";
+    public static final String VolunteerAlredyAcceptedRecruitment = "해당 봉사자는 해당 날짜에 이미 승인 완료된 봉사가 존재합니다.";
 }
