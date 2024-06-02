1a5b4315396e7657503180d1f448810d8a688e07
minju <minju9711@naver.com>
2024-06-02 22:53:29 +0900

    remove : 지워진 클래스

---
+++ back/src/main/java/Challengesemester2024/domain/childCenter/dto/get/ResponseChildCenterRecruitment.java
@@ -0,0 +1,13 @@
+package Challengesemester2024.domain.childCenter.dto.get;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+//@Getter
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class ResponseChildCenterRecruitment {
+//}
