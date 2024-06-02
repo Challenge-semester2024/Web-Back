36d213ebc3e860f89565859291aa692dfd201713
minju <minju9711@naver.com>
2024-06-02 23:12:54 +0900

    feat : 웹의 봉사공고 컨트롤러 클래스 추가

--- back/src/main/java/Challengesemester2024/SpringSecurity/jwt/controller/JwtController.java
+++ back/src/main/java/Challengesemester2024/SpringSecurity/jwt/controller/JwtController.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
