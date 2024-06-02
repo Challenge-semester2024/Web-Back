1eb540454fe09c5bf6877e8b5c01d08e2ac57786
minju <minju9711@naver.com>
2024-06-02 22:50:36 +0900

    feat : 특정한 주소, 이름으로 보육원 찾는 함수 추가

--- back/src/main/java/Challengesemester2024/domain/childCenter/repository/ChildCenterRepository.java
+++ back/src/main/java/Challengesemester2024/domain/childCenter/repository/ChildCenterRepository.java
@@ -1,10 +1,19 @@
 package Challengesemester2024.domain.childCenter.repository;
 
 import Challengesemester2024.domain.childCenter.model.ChildCenter;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChildCenterRepository extends JpaRepository<ChildCenter, Long>, ChildCenterRepositoryCustom{
     Optional<ChildCenter> findByPhoneNumId(String phoneNumId);
+
+    @Query("SELECT c FROM ChildCenter c WHERE c.roadAddress LIKE %:keyword%")
+    List<ChildCenter> findByRoadAddressContaining(@Param("keyword") String keyword);
+
+    @Query("SELECT c FROM ChildCenter c WHERE c.centerName LIKE %:keyword%")
+    List<ChildCenter> findByCenterNameContaining(@Param("keyword") String keyword);
 }
