6ae3962aaaa11b5cb541ac33233dcf0fc36d6bc7
minju <minju9711@naver.com>
2024-06-02 22:48:31 +0900

    feat : 찾는 봉사기관이 없을 경우에 해당하는 에러처리 클래스

--- back/src/main/java/Challengesemester2024/Exception/collections/business/CenterNotFoundException.java
+++ back/src/main/java/Challengesemester2024/Exception/collections/business/CenterNotFoundException.java
@@ -1,11 +1,9 @@
 package Challengesemester2024.Exception.collections.business;
 
-import Challengesemester2024.Exception.collections.business.BusinessException;
 import Challengesemester2024.Exception.message.CommonExceptionMessage;
 
-public class CenterByManagerNotFoundException extends BusinessException {
-    public CenterByManagerNotFoundException() {
+public class CenterNotFoundException extends BusinessException {
+    public CenterNotFoundException() {
         super(CommonExceptionMessage.CenterByManagerNotFound );
     }
-
 }
