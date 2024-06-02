53cad470be0b60a165083da4576db9760b39ce41
minju <minju9711@naver.com>
2024-06-02 22:48:51 +0900

    feat : 찾는 보육원이 이미 있는 경우에 해당하는 에러처리 클래스

--- back/src/main/java/Challengesemester2024/Exception/collections/business/ChildCenterAlreadyExitsException.java
+++ back/src/main/java/Challengesemester2024/Exception/collections/business/ChildCenterAlreadyExitsException.java
@@ -1,6 +1,5 @@
 package Challengesemester2024.Exception.collections.business;
 
-import Challengesemester2024.Exception.collections.business.BusinessException;
 import Challengesemester2024.Exception.message.CommonExceptionMessage;
 
 public class ChildCenterAlreadyExitsException extends BusinessException {
