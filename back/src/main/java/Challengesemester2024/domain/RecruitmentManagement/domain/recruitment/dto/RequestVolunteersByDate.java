b40f4b0dee16eea2b14261343042da562b0a702d
minju <minju9711@naver.com>
2024-06-04 17:59:48 +0900

    feat : 특정 날짜에 대한 봉사 공고의 정보 요청하는 dto 생성

---
+++ back/src/main/java/Challengesemester2024/domain/RecruitmentManagement/domain/recruitment/dto/RequestVolunteersByDate.java
@@ -0,0 +1,18 @@
+package Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestVolunteersByDate {
+    @NotNull
+    private Long recruitmentId;
+    @NotNull
+    private LocalDate localDate;
+}
