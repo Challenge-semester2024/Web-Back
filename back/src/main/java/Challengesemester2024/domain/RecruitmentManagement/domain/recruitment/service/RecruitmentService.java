ae4b9e2bde5708833c085501a752783f9f43dfba
minju <minju9711@naver.com>
2024-06-04 17:55:23 +0900

    feat : 봉사공고 업데이트 요청 로직 생성 feat : 제목으로 봉사 검색 요청 로직 생성

--- back/src/main/java/Challengesemester2024/domain/RecruitmentManagement/domain/recruitment/service/RecruitmentService.java
+++ back/src/main/java/Challengesemester2024/domain/RecruitmentManagement/domain/recruitment/service/RecruitmentService.java
@@ -1,23 +1,19 @@
 package Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.service;
 
 
-import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto.RecruitmentDetailDto;
-import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto.RecruitmentSummaryDto;
-import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto.RequestRecruitmentDto;
+import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto.*;
 import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.model.Recruitment;
 import Challengesemester2024.domain.childCenter.model.ChildCenter;
-import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.dto.RecruitmentPageDto;
-import Challengesemester2024.domain.volunteer.model.Volunteer;
 import org.springframework.data.domain.Pageable;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface RecruitmentService {
     void createRecruitment(RequestRecruitmentDto requestRecruitmentDto, ChildCenter childCenterPk);
-    RecruitmentPageDto getRecruitments(Pageable pageable);
-    List<Recruitment> getRecruitmentsByChildCenter(ChildCenter childCenter);
+    void updateRecruitment(RequestRecruitmentUpdateDto requestRecruitmentUpdateDto, ChildCenter childCenter);
+    RecruitmentPageDto getRecruitments(Pageable pageable, ChildCenter fetchedChildCenter);
     List<RecruitmentSummaryDto> getRecruitmentSummariesByChildCenter(ChildCenter childCenter);
     Recruitment getRecruitmentById(Long id);
     RecruitmentDetailDto getRecruitmentDetail(Long id);
+    RecruitmentPageDto findByNameWithPagination(String recruitmentName, Pageable pageable, ChildCenter fetchedChildCenter);
 }
