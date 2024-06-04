0f5a58f0126c48eab196afea35fe70650f823c4b
minju <minju9711@naver.com>
2024-06-04 17:53:55 +0900

    modify : 동시 사용자의 인가 확인 위한 로직 추가 feat : feat : 제목으로 봉사 검색 요청 로직 생성

--- back/src/main/java/Challengesemester2024/domain/RecruitmentManagement/domain/recruitment/repository/RecruitmentRepositoryCustom.java
+++ back/src/main/java/Challengesemester2024/domain/RecruitmentManagement/domain/recruitment/repository/RecruitmentRepositoryCustom.java
@@ -1,9 +1,11 @@
 package Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.repository;
 
 import Challengesemester2024.domain.RecruitmentManagement.domain.recruitment.model.Recruitment;
+import Challengesemester2024.domain.childCenter.model.ChildCenter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface RecruitmentRepositoryCustom {
-    Page<Recruitment> findAllWithPagination(Pageable pageable);
+    Page<Recruitment> findAllWithPagination(ChildCenter childCenter, Pageable pageable);
+    Page<Recruitment> findByNameWithPagination(String recruitmentName, ChildCenter fetchedChildCenter, Pageable pageable);
 }
