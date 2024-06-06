dd2cb60d15374c5c417cfe225f6e9b4fd0961046
minju <minju9711@naver.com>
2024-06-06 19:35:16 +0900

    modify : 해당 보육원 관심여부 담을 필드 추가

--- back/src/main/java/Challengesemester2024/domain/center/childCenter/dto/put/ResponseChildCenterDetailDto.java
+++ back/src/main/java/Challengesemester2024/domain/center/childCenter/dto/put/ResponseChildCenterDetailDto.java
@@ -1,19 +1,19 @@
-package Challengesemester2024.domain.childCenter.dto.put;
+package Challengesemester2024.domain.center.childCenter.dto.put;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResponseChildCenterDto {
+public class ResponseChildCenterDetailDto {
     private Long id;
     private String centerName;
     private String roadAddress;
     private String detailAddress;
     private String phoneNumber;
+    private boolean isLike;
 }
