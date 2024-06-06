c47ea1b477b5d7bf58bbd0302060384e854fd41b
minju <minju9711@naver.com>
2024-06-06 19:30:23 +0900

    feat : 관심 등록 보육원 등록 완료시 반환 메시지 추가 feat : 봉사 공고 스크랩 완료시 반환 메시지 추가

--- back/src/main/java/Challengesemester2024/config/constant/ControllerConstants.java
+++ back/src/main/java/Challengesemester2024/config/constant/ControllerConstants.java
@@ -9,9 +9,9 @@ public class ControllerConstants {
     public static final String completecreateRecruitment = "봉사공고 생성이 완료되었습니다.";
     public static final String completeupdateRecruitment = "봉사공고 수정이 완료되었습니다.";
     public static final String completecreateRecruitmentWaiting = "해당 봉사 신청이 완료되었습니다.";
-    public static final String completeFindByName = "성공적으로 봉사공고를 검색했습니다.";
-    public static final String completeGetVolunteersByDate = "해당 날짜의 봉사자를 성공적으로 검색했습니다.";
     public static final String completeAcceptVolunteer = "봉사공고 신청이 승인되었습니다.";
     public static final String completeDeleteAcceptanceVolunteer = "해당 승인자의 봉사공고 신청이 삭제되었습니다.";
     public static final String completeDeleteWaitingVolunteer = "해당 대기자의 봉사공고 신청이 삭제되었습니다.";
+    public static final String completeAddLikeCenter = "관심 등록 보육원으로 등록 완료 되었습니다.";
+    public static final String completeScrapRecruitment = "해당 공고글 스크랩 완료 되었습니다.";
 }
